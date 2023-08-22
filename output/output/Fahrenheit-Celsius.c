1cf43cf59ffa1895b32e4c2378987289032b5237
Rubi Leandro <rubi@brislandventures.com>
2023-08-22 11:39:53 +0400

    more compact

--- output/output/Fahrenheit-Celsius.c
+++ output/output/Fahrenheit-Celsius.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-/* Print Fahrenheit-Celsius table for f = 0, 20, ..., 300 */
+main() /* Fahrenheit-Celsius table */
+{
+    int fahr;
 
-int main() {
-    int lower, upper, step;
-    float fahr, celsius;
+    printf("Fahrenheit to Celsius Converter\n");
 
-    lower = 0;     /* lower limit */
-    upper = 300;   /* upper limit */
-    step = 20;     /* step size */
-    fahr = lower;
-
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-
-    return 0;
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
