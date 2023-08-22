b1cf4f6c3b492b46b4ac7c86830de09d8054d165
Rubi Leandro <rubi@brislandventures.com>
2023-08-22 19:03:31 +0400

    Using Symbolic Constants

--- output/output/Fahrenheit-Celsius.c
+++ output/output/Fahrenheit-Celsius.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
+
 main() /* Fahrenheit-Celsius table */
 {
+    printf("Fahrenheit to Celsius Converter\n");
+
     int fahr;
 
-    printf("Fahrenheit to Celsius Converter\n");
-    
-    // /* Original */
-    // for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-    //     printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    /* Reverse order */
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
-    printf("%4d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
-    
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
