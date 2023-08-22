cb7ebd1a9237faf41f9c06f84310ec92bbcb058c
Rubi Leandro <rubi@brislandventures.com>
2023-08-23 03:49:43 +0400

    exercise 1-6

--- line_count.c
+++ line_count.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-main() /* count lines in input */
+int main() /* count lines, tabs, and blanks in input */
 {
-    int c, nl;
+    int c, nl, nt, nb;
 
-    nl = 0;
+    nl = 0;   /* count of newline characters */
+    nt = 0;   /* count of tab characters */
+    nb = 0;   /* count of blank space characters */
+
     while ((c = getchar()) != EOF)
+    {
         if (c == '\n')
             ++nl;
-    printf("%d\n", nl);
+        else if (c == '\t')
+            ++nt;
+        else if (c == ' ')
+            ++nb;
+    }
+
+    printf("Newlines: %d\n", nl);
+    printf("Tabs: %d\n", nt);
+    printf("Blanks: %d\n", nb);
+
+    return 0;
 }
