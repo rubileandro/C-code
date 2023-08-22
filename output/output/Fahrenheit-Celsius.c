e258310179ee9186c03e897ab70cfc2f5e37a41c
Rubi Leandro <rubi@brislandventures.com>
2023-08-22 11:50:33 +0400

    reverse order

--- output/output/Fahrenheit-Celsius.c
+++ output/output/Fahrenheit-Celsius.c
@@ -5,7 +5,13 @@ main() /* Fahrenheit-Celsius table */
     int fahr;
 
     printf("Fahrenheit to Celsius Converter\n");
+    
+    // /* Original */
+    // for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    //     printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    /* Reverse order */
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf("%4d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    
 }
